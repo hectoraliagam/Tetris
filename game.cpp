7ed5d1a76ab0b54124bc0fdaf5c752a1555fb343
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-29 13:45:02 -0500

    rotate the blocks

--- game.cpp
+++ game.cpp
@@ -46,6 +46,9 @@ void Game::HandleInput() {
 	case KEY_DOWN:
 		MoveBlockDown();
 		break;
+	case KEY_UP:
+		RotateBlock();
+		break;
 	}
 }
 
@@ -78,4 +81,11 @@ bool Game::IsBlockOutside() {
 		}
 	}
 	return false;
+}
+
+void Game::RotateBlock() {
+	currentBlock.Rotate();
+	if (IsBlockOutside()) {
+		currentBlock.UndoRotation();
+	}
 }
