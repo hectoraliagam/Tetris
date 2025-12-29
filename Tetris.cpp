74d798a4b340321a102df23bc4505d4569ac5f89
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-29 10:04:00 -0500

    creating the grid

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,14 +1,22 @@
 #include "raylib.h"
+#include "grid.h"
 
 int main()
 {
-	Color darkBlue = { 41, 41, 120, 255 };
+	Color darkBlue = { 44, 44, 127, 255 };
 	InitWindow(300, 600, "Tetris with Raylib");
 	SetTargetFPS(60);
+
+	Grid grid = Grid();
+	grid.grid[0][0] = 1;
+	grid.grid[3][5] = 4;
+	grid.grid[17][8] = 7;
+	grid.Print();
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(darkBlue);
+		grid.Draw();
 
 		EndDrawing();
 	}
