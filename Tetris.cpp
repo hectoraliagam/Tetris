b1aa3fe3389158b04678afdd5c86c915fff7eae2
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-29 14:23:55 -0500

    checking for collisions

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,6 +1,17 @@
 #include <raylib.h>
 #include "game.h"
 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval) {
+	double currentTime = GetTime();
+	if (currentTime - lastUpdateTime >= interval) {
+		lastUpdateTime = currentTime;
+		return true;
+	}
+	return false;
+}
+
 int main()
 {
 	Color darkBlue = { 44, 44, 127, 255 };
@@ -11,10 +22,13 @@ int main()
 
 	while (!WindowShouldClose()) {
 		game.HandleInput();
+		if (EventTriggered(0.2)) {
+			game.MoveBlockDown();
+		}
+
 		BeginDrawing();
 		ClearBackground(darkBlue);
 		game.Draw();
-
 		EndDrawing();
 	}
 
--- game.cpp
+++ game.cpp
@@ -54,22 +54,23 @@ void Game::HandleInput() {
 
 void Game::MoveBlockLeft() {
 	currentBlock.Move(0, -1);
-	if (IsBlockOutside()) {
+	if (IsBlockOutside() || !BlockFits()) {
 		currentBlock.Move(0, 1);
 	}
 }
 
 void Game::MoveBlockRight() {
 	currentBlock.Move(0, 1);
-	if (IsBlockOutside()) {
+	if (IsBlockOutside() || !BlockFits()) {
 		currentBlock.Move(0, -1);
 	}
 }
 
 void Game::MoveBlockDown() {
 	currentBlock.Move(1, 0);
-	if (IsBlockOutside()) {
+	if (IsBlockOutside() || !BlockFits()) {
 		currentBlock.Move(-1, 0);
+		LockBlock();
 	}
 }
 
@@ -85,7 +86,27 @@ bool Game::IsBlockOutside() {
 
 void Game::RotateBlock() {
 	currentBlock.Rotate();
-	if (IsBlockOutside()) {
+	if (IsBlockOutside() || !BlockFits()) {
 		currentBlock.UndoRotation();
 	}
+}
+
+void Game::LockBlock() {
+	std::vector<Position> tiles = currentBlock.GetCellPositions();
+	for (Position item : tiles) {
+		grid.grid[item.row][item.column] = currentBlock.id;
+	}
+	currentBlock = nextBlock;
+	nextBlock = GetRandomBlock();
+}
+
+bool Game::BlockFits()
+{
+	std::vector<Position> tiles = currentBlock.GetCellPositions();
+	for (Position item : tiles) {
+		if (grid.IsCellEmpty(item.row, item.column) == false) {
+			return false;
+		}
+	}
+	return true;
 }
